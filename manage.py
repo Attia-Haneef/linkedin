66beb9e11ec1d2b8206544fd8c712511e4fa1f74
Attia Haneef <87973611+attiahaneef-qbatch@users.noreply.github.com>
2023-09-26 16:14:48 +0500

    ee

--- manage.py
+++ manage.py
@@ -1,1 +1,1 @@
-Apologies, but without the code that needs refactoring, I cannot provide a complete solution. Could you please provide the code so that I can refactor it for you?
+No problem! Please provide the code that needs to be refactored, and I'll be happy to help you with the refactoring process.
